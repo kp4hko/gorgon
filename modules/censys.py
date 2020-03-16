f17fa616f97ff0453dcd3f0027c3799924c1da38
kp4hko <kp4hko@live.com>
2020-03-16 21:08:16 +0000

    added censys, improved resolving

---
+++ modules/censys.py
@@ -0,0 +1,30 @@
+import censys.ipv4
+import json
+from modules.common import get_top_level_domains, get_config_param
+import re
+
+def run(db_connection, project_name):
+	top_domains = get_top_level_domains(db_connection, project_name)
+	if top_domains is not None:
+		censys_keys = get_config_param("censys", "path to keys")
+		with open(censys_keys) as json_file:
+			api_keys = json.load(json_file)
+			censys_conn = censys.ipv4.CensysIPv4(api_id=api_keys["censysUsername"], api_secret=api_keys["censysSecret"])
+			for top_domain in top_domains:
+				domain_to_search = re.sub('\.', '\\.', top_domain)
+				try:
+					for censys_search_result in censys_conn.search("443.https.tls.certificate.parsed.extensions.subject_alt_name.dns_names: /.*\." + domain_to_search + "/", ['ip', '443.https.tls.certificate.parsed.extensions.subject_alt_name.dns_names']):
+						host = db_connection[project_name + ".ips"].find_one({ "ip": censys_search_result['ip']})
+						certs = censys_search_result['443.https.tls.certificate.parsed.extensions.subject_alt_name.dns_names']
+						if host is None:
+							db_connection[project_name + ".ips"].insert_one({ "ip": censys_search_result['ip'] })
+							host = db_connection[project_name + ".ips"].find_one({ "ip": censys_search_result['ip']})
+
+						for cert in certs:
+							if 'certs' in host:
+								if cert not in host['certs']:
+									db_connection[project_name + ".ips"].update_one({"ip": censys_search_result['ip']}, {'$push': {"certs": cert}})
+							else:
+								db_connection[project_name + ".ips"].update_one({"ip": censys_search_result['ip']}, {'$set': {"certs": [cert] }})
+				except Exception as e:
+					print(e.__class__, e)
--- modules/resolve_unresolved_hosts.py
+++ modules/resolve_unresolved_hosts.py
@@ -1,7 +1,33 @@
-from modules.common import run_massdns
+from modules.common import run_massdns, get_top_level_domains
 
 def run(db_connection, project_name):
-	subdomains_to_test = []
-	for domain in db_connection[project_name + ".domains"].find({'ip': {'$exists': False}, 'cname': {'$exists': False}}):
-		subdomains_to_test.append(domain["domain"])
-	run_massdns(db_connection, project_name, subdomains_to_test, False)
+	top_domains = get_top_level_domains(db_connection, project_name)
+	if top_domains is not None:
+		subdomains_to_test = []
+		for domain in db_connection[project_name + ".domains"].find({'ip': {'$exists': False}, 'cname': {'$exists': False}}):
+			subdomains_to_test.append(domain["domain"])
+		if len(subdomains_to_test) > 0:
+			run_massdns(db_connection, project_name, subdomains_to_test, False)
+		company_cnames = find_cnames_pointing_to_company(db_connection, top_domains, project_name)
+		while len(company_cnames) > 0:
+			run_massdns(db_connection, project_name, company_cnames, False)
+			company_cnames = find_cnames_pointing_to_company(db_connection, top_domains, project_name)
+
+def find_cnames_pointing_to_company(db_connection, top_domains, project_name):
+	cnames_pointing_to_company = []
+	for domain in db_connection[project_name + ".domains"].find({'ip': {'$exists': False}, 'cname': {'$exists': True}}):
+		cname_to_check = domain["cname"]
+		for top_domain in top_domains:
+			if cname_to_check.endswith(top_domain + '.'):
+				if cname_to_check not in cnames_pointing_to_company:
+					cnames_pointing_to_company.append(cname_to_check)
+				break
+	cnames_to_test = []
+	for cname in cnames_pointing_to_company:
+		correct_cname = cname.strip('.')
+		cname_to_test = db_connection[project_name + ".domains"].find_one({'domain': correct_cname})
+		if cname_to_test == None:
+			cnames_to_test.append(correct_cname)
+			db_connection[project_name + ".domains"].insert_one({'domain': correct_cname})
+	return cnames_to_test
+
